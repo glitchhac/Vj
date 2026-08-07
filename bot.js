8a1c4c74682de8d029a02199f77ecffa444b5372
glitchhac <2evoacc667788@gmail.com>
2026-08-08 03:36:21 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,7 +100,7 @@ const isOwner = (userId) => adminIDs.includes(String(userId));
 
 // ========== CHECK CHANNELS FUNCTION ==========
 const checkUserJoinedChannels = async (userId) => {
-  const channels = ['@shadowofficial786', '@shadowbanproof'];
+  const channels = ['@skxwaji', '@skxwajibanproof'];
   let allJoined = true;
 
   for (const channel of channels) {
@@ -126,9 +126,9 @@ const sendChannelsRequiredMessage = async (chatId) => {
       parse_mode: 'Markdown',
       reply_markup: {
         inline_keyboard: [
-          [{ text: '📢 Channel 1', url: 'https://t.me/shadowofficial786' }],
-          [{ text: '📢 Channel 2', url: 'https://t.me/shadowbanproof' }],
-          [{ text: '👥 Group', url: 'https://t.me/skchatzone' }],
+          [{ text: '📢 Channel 1', url: 'https://t.me/skxwaji' }],
+          [{ text: '📢 Channel 2', url: 'https://t.me/skxwajibanproof' }],
+          [{ text: '👥 Group', url: 'https://t.me/skxwajibug' }],
           [{ text: '✅ I have joined', callback_data: 'check_join' }]
         ]
       }
